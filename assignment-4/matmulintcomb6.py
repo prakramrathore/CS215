9d0ea8bb2ae6cc3e59390943d50901af580ca1d2
prakramrathore <76489336+prakramrathore@users.noreply.github.com>
2022-04-20 05:07:20 +0530

    Add files via upload

---
+++ assignment-4/matmulintcomb6.py
@@ -0,0 +1,25 @@
+import random
+import time
+	
+n = 128
+m1 = [[0]*n]*n
+m2 = [[0]*n]*n
+
+for i in range(0,n):
+        for j in range(0,n):
+            m1[i][j] = random.randint(0,50)
+            
+for i in range(0,n):
+        for j in range(0,n):
+            m2[i][j] = random.randint(0,50)
+            
+resultant_matrix = [[0]*n]*n
+	
+start = time.time()
+for k in range(0,n):
+	for j in range(0,n):
+		for i in range(0,n):
+			resultant_matrix[i][j] += m1[i][k]*m2[k][j]
+end = time.time()
+meet_portion_time = end-start
+print("Meet Potion Time: ",meet_portion_time, end="\n")
